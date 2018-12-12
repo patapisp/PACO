351acedfbaa14b3e8df62e23ac84a6234edf3036
Evert Nasedkin <nasedkinevert@gmail.com>
2018-12-12 22:53:14 +0100

    Can produce something like an SNR map

--- paco/processing/fullpaco.py
+++ paco/processing/fullpaco.py
@@ -2,6 +2,7 @@ from .. import core
 from paco.util.util import *
 from .paco import PACO
 import matplotlib.pyplot as plt
+import sys
 
 class FullPACO(PACO):
     def __init__(self,                 
@@ -23,12 +24,13 @@ class FullPACO(PACO):
         phi0s = []
         for i in range(self.im_stack.shape[1]):
             for j in range(self.im_stack.shape[2]):
-                phi0s.append(np.array((i,j)))
+                phi0s.append(np.array([i,j]))
 
         
-
-        SNR = self.PACO(phi0s,angles)
-        return SNR
+        a,b = self.PACO(np.array(phi0s),angles)
+        a = np.reshape(a,(self.im_stack.shape[1],self.im_stack.shape[2]))
+        b = np.reshape(b,(self.im_stack.shape[1],self.im_stack.shape[2]))
+        return a,b
     
     def PACO(self, phi0s, angles, model_name=gaussian2d_model):
         # Generate list of initial points to test (how many, where?)
@@ -40,12 +42,9 @@ class FullPACO(PACO):
 
         
         k = self.k
+        
         patch = np.zeros((npx,T,T,2*k,2*k))
         m     = np.zeros((npx,T,2*k,2*k))
-        S     = np.zeros((npx,T,4*k*k,4*k*k))
-        rho   = np.zeros((npx,T,4*k*k,4*k*k))
-        F     = np.zeros((npx,T,4*k*k,4*k*k))
-        C     = np.zeros((npx,T,4*k*k,4*k*k))
         Cinv  = np.zeros((npx,T,4*k*k,4*k*k))
         h     = np.zeros((npx,T,2*k,2*k))
         print("Running PACO...")
@@ -54,42 +53,31 @@ class FullPACO(PACO):
                 print(i)
             dim = (N/2)
             x, y = np.meshgrid(np.arange(-dim, dim), np.arange(-dim, dim))
-            phi0 = (x[p0[0], p0[1]], y[p0[0], p0[1]])           
-            r, theta = cart2pol(x,y)
-            r0, theta0 = cart2pol(p0[0], p0[1])
-                        
-            angles_rad = np.array([a*np.pi/180 for a in angles])+theta0           
-            angles_ind = [[r.flatten()[(np.abs(r.flatten() - r0)).argmin()],
-                           theta.flatten()[(np.abs(theta.flatten() - phi)).argmin()]] for phi in angles_rad]
+            phi0 = np.array([x[p0[0], p0[1]], y[p0[0], p0[1]]])
+            rphi0 = cart_to_pol(phi0)
+            r,theta = grid_cart_to_pol(x,y)
+            angles_rad = np.array([a*np.pi/180 for a in angles])-rphi0[1]  
+            angles_ind = [[-rphi0[0],rphi0[0]-phi] for phi in angles_rad]
             angles_pol = np.array(list(zip(*angles_ind)))
-            angles_px = np.array(int_pol2cart(angles_pol[0], angles_pol[1]))+dim
+            angles_px = np.array(grid_pol_to_cart(angles_pol[0], angles_pol[1]))+dim
             angles_px = angles_px.T
-            
-            #    for l in range(T):
             for j,ang in enumerate(angles_px):
                 patch[i][j] = self.get_patch(ang, k)
                 m[i][j] = np.mean(patch[i][j], axis=0)
-                S[i][j] = self.sample_covariance(patch[i][j], m[i][j], T)
-                rho[i][j] = self.shrinkage_factor(S[i][j], T)
-                F[i][j] = self.diag_sample_covariance(S[i][j])
-                C[i][j] = self.covariance(rho[i][j], S[i][j], F[i][j])
-                Cinv[i][j] = np.linalg.inv(C[i][j])
+                S = self.sample_covariance(patch[i][j], m[i][j], T)
+                rho = self.shrinkage_factor(S, T)
+                F = self.diag_sample_covariance(S)
+                C = self.covariance(rho, S, F)
+                Cinv[i][j] = np.linalg.inv(C)
                 h[i][j] = self.model_function(2*k,model_name,sigma=5)
-
-            print(patch[i][0][0])
-            self.plotting(patch[i][0])
-            self.plotting(C[i])
-            #print("Shapes: m, S, rho, F, C, h")
-            #print(m[i].shape, S[i].shape, rho[i].shape, F[i].shape, C[i].shape, h[i].shape)
+            #self.plotting(patch[i][0])
+            #self.plotting(C[i])
             a[i] = np.sum(self.al(h[i], Cinv[i]),axis=0)
             b[i] = np.sum(self.bl(h[i], Cinv[i], patch[i][j], m[i]), axis=0)
-        #a = np.reshape(a,(self.im_stack.shape[1],self.im_stack.shape[2]))
-        #b = np.reshape(b,(self.im_stack.shape[1],self.im_stack.shape[2]))
         print("Done")
-        return b,a
+        return a,b
 
-    def plotting(self,patch, vmax = 5):
-        
+    def plotting(self,patch, vmax = 5):       
         print(patch.shape)
         dim = len(patch)
         n = int(np.ceil(np.sqrt(dim)))
