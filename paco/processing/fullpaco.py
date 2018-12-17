Fixed shrinkage factor calculation. 'a' matrix seems to be rotated?

paco/processing/fullpaco.py
@@ -19,7 +19,7 @@ class FullPACO(PACO):
     Algorithm Functions
     """
     
-    def PACO(self,angles):
+    def PACO(self,angles, resolution = 1):
         """
         PACO
 
@@ -28,19 +28,19 @@ class FullPACO(PACO):
         true SNR map.
 
         :angles: Array of angles from frame rotation
+        :resolution: Amount of oversampling of image to improve positioning of PSF (don't use yet)
         """
 
         # Setup pixel coordinates
-        x,y = np.meshgrid(np.arange(0,self.im_stack.shape[1]),np.arange(0,self.im_stack.shape[2]))
+        x,y = np.meshgrid(np.arange(0,self.im_stack.shape[1], step = 1/resolution),np.arange(0,self.im_stack.shape[2], step = 1/resolution))
         phi0s = np.column_stack((x.flatten(),y.flatten()))
         # Compute a,b
-        a,b,c = self.PACO_calc(np.array(phi0s),angles)
+        a,b = self.PACO_calc(np.array(phi0s),angles)
 
         # Reshape into a 2D image, with the same dimensions as the input images
         a = np.reshape(a,(self.im_stack.shape[1],self.im_stack.shape[2]))
         b = np.reshape(b,(self.im_stack.shape[1],self.im_stack.shape[2]))
-        c = np.reshape(c,(self.im_stack.shape[1],self.im_stack.shape[2]))
-        return a,b,c
+        return a,b
     
     def PACO_calc(self, phi0s, angles, model_name=gaussian2d_model):
         """
@@ -58,7 +58,6 @@ class FullPACO(PACO):
         npx = len(phi0s)  # Number of pixels in an image
         a = np.zeros(npx) # Setup output arrays
         b = np.zeros(npx)
-        c = np.zeros(npx)
         
         T = len(self.im_stack) # Number of temporal frames
         k = self.k             # Half-width of a patch
@@ -74,8 +73,6 @@ class FullPACO(PACO):
         # Set up coordinates so 0 is at the center of the image                   
         dim = (N/2)
         x, y = np.meshgrid(np.arange(-dim, dim), np.arange(-dim, dim))
-        r,theta = grid_cart_to_pol(x,y)
-        theta = (theta + np.pi)
         # Loop over all pixels
         # i is the same as theta_k in the PACO paper
         for i,p0 in enumerate(phi0s):
@@ -96,33 +93,48 @@ class FullPACO(PACO):
 
             # Convert from polar to cartesian and pixel coordinates
             angles_px = np.array(grid_pol_to_cart(angles_pol[0], angles_pol[1]))+dim
-            #plt.imshow(self.im_stack[0])
-            #plt.plot(angles_px[0], angles_px[1], 'rx')
-            #sys.exit(1)
+            
             # Transpose to tuples
             angles_px = angles_px.T
 
             # Iterate over each temporal frame/each angle
             # Same as iterating over phi_l
             for l,ang in enumerate(angles_px):
+                #fig,ax = plt.subplots(nrows=2,ncols=3,figsize=(12,8))
+                #ax = ax.flatten()
                 patch[i][l] = self.get_patch(ang, k) # Get the column of patches at this point
+                #ax[0].imshow(patch[i][l][l])
+                #ax[0].set_title("Current Patch")
                 m[i][l] = np.mean(patch[i][l], axis=0) # Calculate the mean of the column
-
                 # Calculate the covariance matrix
-                S = self.sample_covariance(patch[i][l], m[i][l], T) 
+                S = self.sample_covariance(patch[i][l], m[i][l], T)
+                #ax[1].imshow(S)
+                #ax[1].set_title("S - Covariance matrix")
                 rho = self.shrinkage_factor(S, T) 
+                #ax[2].imshow(rho)
+                #print(rho)
+                #ax[2].set_title("Shrinkage Factor")
                 F = self.diag_sample_covariance(S)
+                #ax[3].imshow(F)
+                #ax[3].set_title("F - Diagonal of Covariance S")
                 C = self.covariance(rho, S, F)
+                #ax[4].imshow(C)
+                #ax[4].set_title("C - Regularized covariance")
                 Cinv[i][l] = np.linalg.inv(C)
+                #ax[5].imshow(Cinv[i][l])
+                #ax[5].set_title("Inverse Covariance")
                 # Setup the model
                 h[i][l] = self.model_function(2*k,model_name,sigma=5)
 
+                w = np.dot(Cinv[i][l],h[i][l].flatten())
+                #a[i] += np.dot(w.T,h[i][l].flatten())
+                #b[i] += np.dot(w.T,(patch[i][l][l] - m[i][l]).flatten())
+                #sys.exit(1)
             # At this location, calculate a and b    
             a[i] = np.sum(self.al(h[i], Cinv[i]),axis=0)
             b[i] = np.sum(self.bl(h[i], Cinv[i], patch[i], m[i]), axis=0)
-            c[i] = i
         print("Done")
-        return a,b,c
+        return a,b
 
     def plotting(self,patch, vmax = 5):
         # Just a temp function, ignore

paco/processing/paco.py
@@ -130,7 +130,7 @@ class PACO:
         T: Number of temporal frames
         """
         top = (np.trace(np.dot(S,S)) + np.trace(S)**2 - 2*np.sum(np.array([d**2 for d in np.diag(S)])))
-        bot = ((T+1)*(np.trace(np.dot(S,S))**2-np.sum(np.array([d**2 for d in np.diag(S)]))))
+        bot = ((T+1)*(np.trace(np.dot(S,S))-np.sum(np.array([d**2 for d in np.diag(S)]))))
         return top/bot
 
     def al(self,hfl, Cfl_inv):
@@ -149,6 +149,7 @@ class PACO:
         hfl = np.reshape(hfl,(hfl.shape[0],hfl.shape[1]*hfl.shape[2]))
         r_fl = np.reshape(r_fl,(r_fl.shape[0],r_fl.shape[1],r_fl.shape[2]*r_fl.shape[3]))
         m_fl = np.reshape(m_fl,(m_fl.shape[0],m_fl.shape[1]*m_fl.shape[2]))
-        return np.array([np.dot(hfl[i].T, np.dot(Cfl_inv[i], (r_fl[i][i]-m_fl[i]))) for i in range(len(hfl))])
+        b = np.array([np.dot(hfl[i].T, np.dot(Cfl_inv[i], (r_fl[i][i]-m_fl[i]))) for i in range(len(hfl))])
+        return b
 
 