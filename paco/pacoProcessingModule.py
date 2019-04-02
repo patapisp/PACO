c35670830af2e2952957658b05a0915a19f4b6b9
Evert Nasedkin <nasedkinevert@gmail.com>
2019-04-02 09:05:45 +0200

    Setting up Pynpoint wrapper for PACO

---
+++ paco/pacoProcessingModule.py
@@ -0,0 +1,8 @@
+import paco as paco
+from pynpoint.core import ProcessingModule
+
+class PACOModule(ProcessingModule):
+    def __init__(self,
+                 name_in = "PACO"):
+        super(PACOModule,self).__init__(name_in)
+        return
